04723ef1e37ce30020858f346f828ea1222f9d4a
ncsereoka <39672261+ncsereoka@users.noreply.github.com>
2020-10-28 23:15:12 +0200

    Also take into account the connection drop

--- src/digifon/Scheduler.cc
+++ src/digifon/Scheduler.cc
@@ -22,22 +22,22 @@ void Scheduler::initialize() {
     int outputGatesCount = this->gateCount();
     allocatedChannels = new int[outputGatesCount];
 
-    std::vector<int> weights =
-            cStringTokenizer(par("weights").stringValue()).asIntVector();
-    if (weights.size() != outputGatesCount) {
+    std::vector<int> initialWeights = cStringTokenizer(
+            par("initialWeights").stringValue()).asIntVector();
+    if (initialWeights.size() != outputGatesCount) {
         throw cRuntimeError("Weights parameter count not equal to gate count.");
     }
 
     int weightSum = 0;
-    for (std::vector<int>::iterator it = weights.begin(); it != weights.end();
-            ++it) {
+    for (std::vector<int>::iterator it = initialWeights.begin();
+            it != initialWeights.end(); ++it) {
         weightSum += *it;
     }
 
     int newWeightSum = 0;
     int factor = par("radioChannelCount").intValue() / weightSum;
     for (int i = 0; i < outputGatesCount; i++) {
-        int newCount = weights.at(i) * factor;
+        int newCount = initialWeights.at(i) * factor;
         if (newCount < 1) {
             newCount = 1;
         }
@@ -63,8 +63,12 @@ void Scheduler::handleMessage(cMessage *msg) {
 
     for (cModule::GateIterator i(this); !i.end(); i++) {
         cGate *gate = *i;
-        send(generateSchedulerMessage(allocatedChannels[gate->getIndex()]),
-                gate);
+        int gateIndex = gate->getIndex();
+        bool normalUser = gateIndex != par("unluckyUserId").intValue();
+
+        if (normalUser || isConnectionNormal()) {
+            send(generateSchedulerMessage(allocatedChannels[gateIndex]), gate);
+        }
     }
 
     scheduleAt(simTime() + par("schedulingCycleDuration"),
@@ -75,6 +79,13 @@ cMessage* Scheduler::generateSchedulerMessage(int allocatedChannels) {
     SchedulerMessage *schedulerMessage = new SchedulerMessage("controlMessage");
     schedulerMessage->setAllocatedChannels(allocatedChannels);
     return schedulerMessage;
+}
+
+bool Scheduler::isConnectionNormal() {
+    simtime_t currentTime = simTime();
+    bool beforeConnectionLost = currentTime < par("connectionLostSec");
+    bool afterConnectionFound = currentTime > par("connectionFoundSec");
+    return beforeConnectionLost || afterConnectionFound;
 }
 
 }
--- src/digifon/Scheduler.h
+++ src/digifon/Scheduler.h
@@ -22,6 +22,7 @@ protected:
 
 private:
     cMessage* generateSchedulerMessage(int allocatedChannels);
+    bool isConnectionNormal();
 };
 
 }
